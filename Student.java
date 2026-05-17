42d7a2a6e0f677ca41d67657f63ff909c4c9c535
142536qwe123 <10255102545@stu.ecnu.edu.cn>
2026-05-17 20:15:35 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,27 @@
+public class Student extends Person {
+    private String major;
+
+    public Student(int age, Gender gender, Name name, String major) {
+        super(age, gender, name);
+        this.major = major;
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("Hi, how is your homework going?");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Major: " + major;
+    }
+
+    // 实现抽象方法 work
+    @Override
+    public void work() {
+        System.out.println("I am studying " + major + ".");
+    }
+    @Override
+    public void breathe() {   
+        System.out.println("Student breathing fast...");
+}
